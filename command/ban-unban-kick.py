2843b59a58f89ccf0d36dd88881b413a89a32f63
xx08x8 <71730912+xx08x8@users.noreply.github.com>
2023-05-21 15:11:08 +0500

    Add files via upload

---
+++ command/ban-unban-kick.py
@@ -0,0 +1,49 @@
+from disnake.ext import commands
+
+intents = disnake.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# kick user
+@bot.slash_command(description="Kick user: /kick [@user] [reason]")
+async def kick(inter, user: disnake.Member, *, reason = None):
+    if not reason:
+        emb = disnake.Embed(title="", color=inter.author.color)
+        emb.add_field(name="", value=f"<@{user.id}> was kicked <@{inter.author.id}> for a reason **no reason**.", inline=True)
+        await user.kick()
+        await inter.send(embed=emb)
+    else:
+        emb = disnake.Embed(title="", color=inter.author.color)
+        emb.add_field(name="", value=f"<@{user.id}> was kicked <@{inter.author.id}> for a reason **{reason}**.", inline=True)
+        await user.kick(reason=reason)
+        await inter.send(embed=emb)
+
+# ban user
+@bot.slash_command(description="Ban user: /ban [@user] [reason]")
+@commands.has_permissions(ban_members=True)
+async def ban(inter, user: disnake.Member, *, reason=None):
+    if not reason:
+        emb = disnake.Embed(title="", color=inter.author.color)
+        emb.add_field(name="", value=f"<@{user.id}> was banned <@{inter.author.id}> for a reason **no reason**.", inline=True)
+        await user.ban()
+        await inter.send(embed=emb)
+    else:
+        emb = disnake.Embed(title="", color=inter.author.color)
+        emb.add_field(name="", value=f"<@{user.id}> was kicked <@{inter.author.id}> for a reason **{reason}**.", inline=True)
+        await user.ban(reason=reason)
+        await inter.send(embed=emb)
+
+# unban user
+@bot.slash_command(description="Unban user: /unban [@user]")
+@commands.has_permissions(ban_members=True)
+async def unban(inter, *, member):
+    banned_users = inter.guild.bans()
+    member_name, member_discriminator = member.split('#')
+    async for ban_entry in banned_users:
+        user = ban_entry.user
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            emb = disnake.Embed(title="", color=inter.author.color)
+            emb.add_field(name="", value=f"<@{inter.author.id}> unbanned <@{user.id}>", inline=True)
+            await inter.guild.unban(user)
+            await inter.send(embed=emb)
+        return
